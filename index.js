fcbf45483e3f3cf5e18a862fb1423bc7000c8ef6
Anosh Malik <me@ano.sh>
2016-04-02 17:31:15 +0100

    Add getLocationData function

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-var express = require('express'),
-    request = require('request'),
-    app = express();
+var express  = require('express'),
+    request  = require('request'),
+    app      = express(),
+    location,
+    rawLocation,
+    that = this;
+
+var getLocationData = function(address) {
+        var url = "https://maps.googleapis.com/maps/api/geocode/json?&address=" + address;
+        return new Promise(function(resolve, reject) {
+            var data;
+            request({url:url}, function (err, res, body) {
+                if (err) {
+                    return reject(err);
+                } else if (res.statusCode !== 200) {
+                    err = new Error("Unexpected status code: " + res.statusCode);
+                    err.res = res;
+                    return reject(err);
+                }
+                data = JSON.parse(body);
+                that.location = data.formatted_address;
+                data = data.results[0].geometry;
+                that.rawLocation = data;
+                console.log(data);
+                resolve(data);
+            });
+        });
+    };
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
